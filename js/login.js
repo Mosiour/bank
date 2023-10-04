f865a13e396d33878d36c4c28b8bf9843a7b23fb
Mosiour <mosiour.munna@gmail.com>
2023-10-04 07:11:21 +0600

    some update

--- js/login.js
+++ js/login.js
@@ -12,7 +12,7 @@ document.getElementById('btn-submit').addEventListener('click',function(){
     // console.log(email,password);
     //step-4
     //verify email & password
-    if(email === 'munna@gmail.com' && password === '12345'){
+    if(email === 'abc@gmail.com' && password === '12345'){
         window.location.href = 'bank.html';
     }
     else{
